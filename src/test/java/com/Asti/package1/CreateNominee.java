9891175f42aa42bc95f7b74328f40a7cf9567d3c
prasad <prasad@LAPTOP-KH3R9QP0>
2023-09-27 17:13:40 +0530

    Second commit

---
+++ src/test/java/com/Asti/package1/CreateNominee.java
@@ -0,0 +1,11 @@
+package com.Asti.package1;
+
+import org.testng.annotations.Test;
+
+public class CreateNominee {
+
+	@Test
+	public void createnominee() {
+		System.out.println("nominee is created");
+	}
+}
